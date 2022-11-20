6648814dba86d26b9dfcd5a75ea1a4d6fd650468
Oscar <orrp@tecnicoecuador.edu.ec>
2022-11-20 18:46:33 -0500

    segundo commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
 from flaskext.mysql import MySQL
 from datetime import datetime
 import os
@@ -21,6 +21,11 @@ mysql.init_app(app)
 CARPETA= os.path.join('uploads')
 # utilizamo la configuracion para guardar esa ruta pero comu un valor del valor de la variable carpeta
 app.config['CARPETA']=CARPETA
+
+# creamos la url que contenga el nombre de la foto
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'],nombreFoto)
 
 # ruta inicial / que renderice el archivo index.html
 @app.route('/')
@@ -47,32 +52,49 @@ def index():
     # enviamos la informacion a el achivo html con una variable empleados
     return render_template('empleados/index.html' , empleados = empleados)
 
-# ?eleminar registro con el parametro id
-@app.route('/destroy/<int:id>')
-def destroy(id):
 
-    # abre conexion
-     conn = mysql.connect()
-     cursor = conn.cursor()
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html' )
 
-    # intruccion para buscar la foto
-     cursor.execute("SELECT *  FROM empleados WHERE id=%s",id)
-     fila=cursor.fetchall()
-    
-     if app.config['CARPETA'] == fila[0][3]:
-      # removemos la foto selecionada
-        os.remove(os.path.join(app.config['CARPETA'], fila[0][3]))
-    
+@app.route('/store', methods=['POST'])
+def storage():
 
-     print(app.config['CARPETA'])
-     # Ejecuta la instruccion SQL
-     cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
+    # llamamos al modulo request para signar los datos el usuario ingresa en el formulario html
+    _nombre = request.form ['txtNombre']
+    _correo = request.form ['txtCorreo']
 
-     #  cierre la conexion 
-     conn.commit()
+    # al ser un foto se debe solicitar en formato de archivo
+    _foto = request.files ['txtFoto']
 
-     # usamos el modulo redireccionar para ir a la ruta inicial
-     return redirect('/')
+    # !con el modulo time se dara formato de tiempo para el archivo de foto
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+
+    #si hay una fotografia, se adjunta con su nuevo nombre
+    global nuevafoto
+    if _foto.filename !='':
+        nuevafoto = tiempo   + str(_foto.filename)
+        _foto.save("uploads/"+nuevafoto)
+
+    # intruccion sql 
+    #  el %s reemplaza al valor que ingresa el usuario
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    # variable para los datos que vamos a insertar
+    datos=(_nombre,_correo,nuevafoto)
+
+    # se va aconectar a mysql
+    conn = mysql.connect()
+    # cusros es informacion que almacena lo que se ejecuta
+    cursor = conn.cursor()
+
+    # mandamoa a ejecutar la instruccion
+    cursor.execute(sql, datos)
+
+    # cerramos la conexion con un commit
+    conn.commit()
+
+    return redirect('/')
 
 # ? Editar registro con el parametro id
 @app.route('/edit/<int:id>')
@@ -125,15 +147,15 @@ def update():
         nuevafoto = tiempo   + str(_foto.filename)
         _foto.save("uploads/"+nuevafoto)
 
-    # intruccion para buscar la foto
-    cursor.execute("SELECT *  FROM empleados WHERE id=%s",id)
-    fila=cursor.fetchall()
+        # intruccion para buscar la foto
+        cursor.execute("SELECT foto  FROM empleados WHERE id=%s",id)
+        fila=cursor.fetchall()
 
-    # removemos la foto selecionada
-    os.remove(os.path.join(app.config['CARPETA'], fila[0][3]))
-    # actualiza la misma tabla pero solo el nombre de la foto
-    cursor.execute("UPDATE empleados SET foto = %s WHERE id = %s", (nuevafoto,id))
-    conn.commit()
+        # removemos la foto selecionada
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+        # actualiza la misma tabla pero solo el nombre de la foto
+        cursor.execute("UPDATE empleados SET foto = %s WHERE id = %s", (nuevafoto,id))
+        conn.commit()
 
     # mandamoa a ejecutar la instruccion
     cursor.execute(sql, datos)
@@ -143,51 +165,30 @@ def update():
     
     return redirect ('/')
 
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html' )
+# ?eleminar registro con el parametro id
+@app.route('/destroy/<int:id>')
+def destroy(id):
 
-@app.route('/store', methods=['POST'])
-def storage():
+    # abre conexion
+     conn = mysql.connect()
+     cursor = conn.cursor()
 
-    # llamamos al modulo request para signar los datos el usuario ingresa en el formulario html
-    _nombre = request.form ['txtNombre']
-    _correo = request.form ['txtCorreo']
+    # intruccion para buscar la foto
+     cursor.execute("SELECT foto FROM empleados WHERE id=%s",id)
+     fila=cursor.fetchall()
+      # removemos la foto selecionada
+     os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+    
 
-    # al ser un foto se debe solicitar en formato de archivo
-    _foto = request.files ['txtFoto']
+     print(app.config['CARPETA'])
+     # Ejecuta la instruccion SQL
+     cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
 
-    # !con el modulo time se dara formato de tiempo para el archivo de foto
-    now = datetime.now()
-    tiempo = now.strftime("%Y%H%M%S")
+     #  cierre la conexion 
+     conn.commit()
 
-    #si hay una fotografia, se adjunta con su nuevo nombre
-    global nuevafoto
-    if _foto.filename !='':
-        nuevafoto = tiempo   + str(_foto.filename)
-        _foto.save("uploads/"+nuevafoto)
-
-    # intruccion sql 
-    #  el %s reemplaza al valor que ingresa el usuario
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-    # variable para los datos que vamos a insertar
-    datos=(_nombre,_correo,nuevafoto)
-
-    # se va aconectar a mysql
-    conn = mysql.connect()
-    # cusros es informacion que almacena lo que se ejecuta
-    cursor = conn.cursor()
-
-    # mandamoa a ejecutar la instruccion
-    cursor.execute(sql, datos)
-
-    # cerramos la conexion con un commit
-    conn.commit()
-
-    render_template('empleados/index.html')
-
-    return redirect('/')
-
+     # usamos el modulo redireccionar para ir a la ruta inicial
+     return redirect('/')
 
 #
 if __name__ == '__main__':
